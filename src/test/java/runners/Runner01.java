53096c1ec21e9c09fe94aa8ee5afa2a2519b2644
Mete <hasanozyer06@gmail.com>
2022-08-19 22:00:39 +0300

    the commit has been made

--- src/test/java/runners/Runner01.java
+++ src/test/java/runners/Runner01.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@featurebitch",
+        tags = "@Wuniversity",
         dryRun = false
 )
 
--- src/test/java/stepDefinitions/WebUniversityDefinations.java
+++ src/test/java/stepDefinitions/WebUniversityDefinations.java
@@ -1,63 +1,71 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import pages.WebUniversityPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class WebUniversityDefinations {
-    WebUniversityPage webUniversityPage = new WebUniversityPage();
+    WebUniversityPage univ = new WebUniversityPage();
 
     @Then("sayfada assagiIn")
     public void sayfada_assagi_ın() {
+        ReusableMethods.hover(univ.webUniversityLoginPortal);
 
     }
 
     @Then("loginPortal butonuna bas")
     public void login_portal_butonuna_bas() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        univ.webUniversityLoginPortal.click();
     }
 
     @Then("diger windowa gec")
     public void diger_windowa_gec() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        ReusableMethods.switchToWindow("WebDriver | Login Portal");
     }
 
     @Then("{string} ve {string}")
-    public void ve(String arg0, String arg1) {
+    public void ve(String userName, String password) {
+        univ.webUniversityUserNameButton.sendKeys(userName);
+        univ.webUniversityUserNameButton.sendKeys(password);
 
     }
 
     @Then("WebUniversity Login butonuna basar")
     public void web_university_login_butonuna_basar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        univ.webUniversityButton.click();
     }
 
     @Then("Popup'ta cikan yazinin {string} oldugunu test edin")
-    public void popup_ta_cikan_yazinin_oldugunu_test_edin(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void popup_ta_cikan_yazinin_oldugunu_test_edin(String alert) {
+        String pop = Driver.getDriver().switchTo().alert().getText();
+        Assert.assertEquals(pop, alert);
     }
 
     @Then("Ok diyerek Popup'i kapatin")
     public void ok_diyerek_popup_i_kapatin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.getDriver().switchTo().alert().accept();
     }
 
     @Then("ilk sayfaya geri donun")
     public void ilk_sayfaya_geri_donun() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        ReusableMethods.switchToWindow("WebDriverUniversity.com");
+
     }
 
     @Then("ilk sayfaya donuldugunu test edin")
     public void ilk_sayfaya_donuldugunu_test_edin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "WebDriverUniversity.com";
+        Assert.assertEquals(expectedTitle, actualTitle);
     }
 
 
-
+    @Then("biraz {int} saniye bekle")
+    public void birazSaniyeBekle(int bekle) {
+        ReusableMethods.waitFor(4);
+    }
 }
