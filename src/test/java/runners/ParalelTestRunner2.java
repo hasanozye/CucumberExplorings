b84646f7f4efc4fb3c840580100b81673e8d182d
Mete <hasanozyer06@gmail.com>
2022-08-22 19:25:29 +0300

    the commit has been made

---
+++ src/test/java/runners/ParalelTestRunner2.java
@@ -0,0 +1,21 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "src/test/resources/features",
+        plugin = {"html:target/Pcucumber-reports2.html",
+                "json:target/json-reports/Pcucumber2.json",
+                "junit:target/xml-report/Pcucumber2.xml"},
+        glue = "stepdefinitions",
+        tags = "@paralel2",
+        dryRun = false
+)
+public class ParalelTestRunner2 {
+
+
+}
